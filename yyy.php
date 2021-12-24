193b36e1efb1fb1d95ea4e54ab24c920856533fa
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2021-12-24 23:30:10 +0800

    Add files via upload

---
+++ yyy.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php001</title>
+    </head>
+    <body>
+        <?php 
+            $text  =  "Hello World!"; 
+            $x  = 10;
+            $y = 1.2;
+            echo $x;
+            echo '<br/>';
+            echo $y + $x;
+            echo '<br/>';
+            echo  $y . $text;
+        ?>
+        
+        
+        
+    </body>
+</html>
