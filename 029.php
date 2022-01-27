261cfeb55fdd3a723940106fce2a98f80d3b6982
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-01-27 23:53:13 +0800

    Add files via upload

---
+++ 029.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="zh">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="author" content="Hsuan_Chang (Jason) Teng" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="robots" content="index,follow" />
+        <meat name="rating" content="General" />
+        <meta target="_self" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" type="image/png" href="" />
+        <link rel="bookmark" href="" />
+
+        <title>029</title>
+    </head>
+    <body>
+        <?php 
+            $file = basename($_SERVER["PHP_SELF"],".php");
+            $path = realpath($file.".php"); // 取得檔案實際路徑
+            echo "實際路徑: $path<br/>";
+            $parts = pathinfo($path);  // 取得路徑資訊
+            echo "路徑: ".$parts["dirname"]."<br/>";
+            echo "檔名: ".$parts["basename"]."<br/>";
+            echo "副檔名: ".$parts["extension"]."<hr/>";
+            // 檢查檔案是否存在
+            if ( file_exists($file. ".php") )
+                    print "檔案: $file.php 存在<br/>";
+            else  print "檔案: $file.php 不存在<br/>";
+            // 複製檔案
+            if (!copy($file. ".php", $file. ".bak")) 
+                print ("檔案: $file.php複製成bak失敗<br/>");
+            else
+                print "檔案: $file.php 複製成bak成功<br/>";
+            rename($file. ".bak", "test.txt");  // 檔案更名
+            print "檔案: $file.bak 更名成test.txt<br/>";
+            $file = "test.txt";
+            unlink($file);   // 刪除檔案
+            print "檔案: $file 已經刪除<br/>";
+        ?>
+
+
+
+    </body>
+</html>
